f6c94e56e85abdc0c55f0bb044997c970a7d2f55
Gilbert Kwabena Akwaboah <gkakwaboah001@gmail.com>
2023-07-13 14:50:09 -0700

    added description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * return - Always 0
  */
 
-int main(void)
+int main(void)	/*no arguments needed*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0);	/*indicate normal exit*/
 }
