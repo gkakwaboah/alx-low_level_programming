ce34ccc86cbcc906c2955f27333d83e117cd2bcb
Gilbert Kwabena Akwaboah <gkakwaboah001@gmail.com>
2023-08-10 11:03:22 -0700

    woow

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,86 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <string.h>
+
+/**
+ * isNumber - checks if a string is a number
+ * @s: string to check
+ * Return: 1 if number, 0 if not
+ */
+int isNumber(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	return (1);
+}
+
+/**
+ * print_result - prints the result of the multiplication
+ * @result: array of integers
+ * @len: length of result
+ * Return: void
+ */
+void print_result(int *result, int len)
+{
+	int i;
+
+	for (i = 0; i < len && result[i] == 0; i++)
+		;
+
+	if (i == len)
+		printf("0");
+
+	for (; i < len; i++)
+		printf("%d", result[i]);
+
+	printf("\n");
+}
 
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
-
-int main(int argc, char *argv[])
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: 0
+ */
+int main(int ac, char **av)
 {
-	unsigned long mul;
-	int i, j;
+	int i, j, num1_len, num2_len;
+	int *result;
 
-	if (argc != 3)
+	if (ac != 3 || !isNumber(av[1]) || !isNumber(av[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	for (i = 1; i < argc; i++)
+	num1_len = strlen(av[1]);
+	num2_len = strlen(av[2]);
+
+	result = calloc(num1_len + num2_len, sizeof(int));
+	if (result == NULL)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = num1_len - 1; i >= 0; i--)
+	{
+		for (j = num2_len - 1; j >= 0; j--)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			int mul = (av[1][i] - '0') * (av[2][j] - '0');
+
+			int sum = result[i + j + 1] + mul;
+
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
 		}
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+
+	print_result(result, num1_len + num2_len);
+	free(result);
 	return (0);
 }
